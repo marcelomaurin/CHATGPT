8569631c56b903f62ba97c66df8e59641924f35d
Marcelo Maurin <Mmaurin@saude.lan>
2026-06-03 16:23:10 -0300

    feat: add hierarchical avatar crash test dummy demo showing skeletal forward kinematics and joint rotation

--- pacote/AI Graphic/aiskeletonrig.pas
+++ pacote/AI Graphic/aiskeletonrig.pas
@@ -5,19 +5,38 @@ unit aiskeletonrig;
 interface
 
 uses
-  Classes, SysUtils, aibase;
+  Classes, SysUtils, Math, aibase;
 
 type
+  TMatrix3x3 = array[0..2, 0..2] of Double;
+
+  TBoneJoint = record
+    Name: string;
+    ParentIndex: Integer;
+    OffsetX, OffsetY, OffsetZ: Double; // Relative offset from parent joint
+    AngleX, AngleY, AngleZ: Double;     // Rotation angles in degrees
+    StartX, StartY, StartZ: Double;     // Computed global start position
+    EndX, EndY, EndZ: Double;           // Computed global end position
+    GlobalRot: TMatrix3x3;              // Accumulated global rotation matrix
+  end;
+
   { TAISkeletonRig }
 
   TAISkeletonRig = class(TAIBaseComponent)
   private
     FBonesList: TStrings;
+    FJoints: array of TBoneJoint;
     procedure SetBonesList(AValue: TStrings);
+    procedure InitializeRig;
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
     procedure RotateBone(const ABoneName: string; const Angle: Double);
+    procedure SetBoneRotation(const ABoneName: string; const AX, AY, AZ: Double);
+    procedure UpdateFK;
+    
+    function GetJointCount: Integer;
+    function GetJoint(Index: Integer): TBoneJoint;
   published
     property BonesList: TStrings read FBonesList write SetBonesList;
   end;
@@ -37,15 +56,9 @@ constructor TAISkeletonRig.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
   FCategory := ccOther;
-  FPrompt := 'Component TAISkeletonRig manages rigging joints and bone hierarchies. Properties: BonesList. Methods: RotateBone.';
+  FPrompt := 'Component TAISkeletonRig manages rigging joints and bone hierarchies. Properties: BonesList. Methods: RotateBone, SetBoneRotation, UpdateFK.';
   FBonesList := TStringList.Create;
-  FBonesList.Add('root');
-  FBonesList.Add('spine');
-  FBonesList.Add('head');
-  FBonesList.Add('left_arm');
-  FBonesList.Add('right_arm');
-  FBonesList.Add('left_leg');
-  FBonesList.Add('right_leg');
+  InitializeRig;
   ClearError;
 end;
 
@@ -55,6 +68,96 @@ begin
   inherited Destroy;
 end;
 
+procedure TAISkeletonRig.InitializeRig;
+begin
+  SetLength(FJoints, 11);
+  
+  // 0. Pelvis/Root
+  FJoints[0].Name := 'pelvis';
+  FJoints[0].ParentIndex := -1;
+  FJoints[0].OffsetX := 0; FJoints[0].OffsetY := 0.2; FJoints[0].OffsetZ := 0;
+  FJoints[0].AngleX := 0; FJoints[0].AngleY := 0; FJoints[0].AngleZ := 0;
+  
+  // 1. Spine (child of Pelvis)
+  FJoints[1].Name := 'spine';
+  FJoints[1].ParentIndex := 0;
+  FJoints[1].OffsetX := 0; FJoints[1].OffsetY := 0.5; FJoints[1].OffsetZ := 0;
+  FJoints[1].AngleX := 0; FJoints[1].AngleY := 0; FJoints[1].AngleZ := 0;
+  
+  // 2. Head (child of Spine)
+  FJoints[2].Name := 'head';
+  FJoints[2].ParentIndex := 1;
+  FJoints[2].OffsetX := 0; FJoints[2].OffsetY := 0.3; FJoints[2].OffsetZ := 0;
+  FJoints[2].AngleX := 0; FJoints[2].AngleY := 0; FJoints[2].AngleZ := 0;
+  
+  // Left Arm
+  // 3. Left Shoulder (child of Spine)
+  FJoints[3].Name := 'left_shoulder';
+  FJoints[3].ParentIndex := 1;
+  FJoints[3].OffsetX := -0.3; FJoints[3].OffsetY := 0; FJoints[3].OffsetZ := 0;
+  FJoints[3].AngleX := 0; FJoints[3].AngleY := 0; FJoints[3].AngleZ := 0;
+  
+  // 4. Left Elbow (child of Left Shoulder)
+  FJoints[4].Name := 'left_elbow';
+  FJoints[4].ParentIndex := 3;
+  FJoints[4].OffsetX := -0.4; FJoints[4].OffsetY := 0; FJoints[4].OffsetZ := 0;
+  FJoints[4].AngleX := 0; FJoints[4].AngleY := 0; FJoints[4].AngleZ := 0;
+  
+  // Right Arm
+  // 5. Right Shoulder (child of Spine)
+  FJoints[5].Name := 'right_shoulder';
+  FJoints[5].ParentIndex := 1;
+  FJoints[5].OffsetX := 0.3; FJoints[5].OffsetY := 0; FJoints[5].OffsetZ := 0;
+  FJoints[5].AngleX := 0; FJoints[5].AngleY := 0; FJoints[5].AngleZ := 0;
+  
+  // 6. Right Elbow (child of Right Shoulder)
+  FJoints[6].Name := 'right_elbow';
+  FJoints[6].ParentIndex := 5;
+  FJoints[6].OffsetX := 0.4; FJoints[6].OffsetY := 0; FJoints[6].OffsetZ := 0;
+  FJoints[6].AngleX := 0; FJoints[6].AngleY := 0; FJoints[6].AngleZ := 0;
+  
+  // Left Leg
+  // 7. Left Hip (child of Pelvis)
+  FJoints[7].Name := 'left_hip';
+  FJoints[7].ParentIndex := 0;
+  FJoints[7].OffsetX := -0.15; FJoints[7].OffsetY := -0.5; FJoints[7].OffsetZ := 0;
+  FJoints[7].AngleX := 0; FJoints[7].AngleY := 0; FJoints[7].AngleZ := 0;
+  
+  // 8. Left Knee (child of Left Hip)
+  FJoints[8].Name := 'left_knee';
+  FJoints[8].ParentIndex := 7;
+  FJoints[8].OffsetX := 0; FJoints[8].OffsetY := -0.5; FJoints[8].OffsetZ := 0;
+  FJoints[8].AngleX := 0; FJoints[8].AngleY := 0; FJoints[8].AngleZ := 0;
+  
+  // Right Leg
+  // 9. Right Hip (child of Pelvis)
+  FJoints[9].Name := 'right_hip';
+  FJoints[9].ParentIndex := 0;
+  FJoints[9].OffsetX := 0.15; FJoints[9].OffsetY := -0.5; FJoints[9].OffsetZ := 0;
+  FJoints[9].AngleX := 0; FJoints[9].AngleY := 0; FJoints[9].AngleZ := 0;
+  
+  // 10. Right Knee (child of Right Hip)
+  FJoints[10].Name := 'right_knee';
+  FJoints[10].ParentIndex := 9;
+  FJoints[10].OffsetX := 0; FJoints[10].OffsetY := -0.5; FJoints[10].OffsetZ := 0;
+  FJoints[10].AngleX := 0; FJoints[10].AngleY := 0; FJoints[10].AngleZ := 0;
+  
+  FBonesList.Clear;
+  FBonesList.Add('pelvis');
+  FBonesList.Add('spine');
+  FBonesList.Add('head');
+  FBonesList.Add('left_shoulder');
+  FBonesList.Add('left_elbow');
+  FBonesList.Add('right_shoulder');
+  FBonesList.Add('right_elbow');
+  FBonesList.Add('left_hip');
+  FBonesList.Add('left_knee');
+  FBonesList.Add('right_hip');
+  FBonesList.Add('right_knee');
+  
+  UpdateFK;
+end;
+
 procedure TAISkeletonRig.SetBonesList(AValue: TStrings);
 begin
   FBonesList.Assign(AValue);
@@ -62,7 +165,135 @@ end;
 
 procedure TAISkeletonRig.RotateBone(const ABoneName: string; const Angle: Double);
 begin
-  Log(llDebug, Format('Rotated bone %s to angle %.2f', [ABoneName, Angle]));
+  SetBoneRotation(ABoneName, 0, 0, Angle);
+end;
+
+procedure TAISkeletonRig.SetBoneRotation(const ABoneName: string; const AX, AY, AZ: Double);
+var
+  I: Integer;
+begin
+  for I := 0 to Length(FJoints) - 1 do
+  begin
+    if SameText(FJoints[I].Name, ABoneName) then
+    begin
+      FJoints[I].AngleX := AX;
+      FJoints[I].AngleY := AY;
+      FJoints[I].AngleZ := AZ;
+      Log(llDebug, Format('Set bone %s rotation to (%.1f, %.1f, %.1f)', [ABoneName, AX, AY, AZ]));
+      UpdateFK;
+      Break;
+    end;
+  end;
+end;
+
+function IdentityMatrix: TMatrix3x3;
+begin
+  FillChar(Result, SizeOf(Result), 0);
+  Result[0, 0] := 1.0;
+  Result[1, 1] := 1.0;
+  Result[2, 2] := 1.0;
+end;
+
+function MultiplyMatrix(const A, B: TMatrix3x3): TMatrix3x3;
+var
+  R, C, K: Integer;
+begin
+  FillChar(Result, SizeOf(Result), 0);
+  for R := 0 to 2 do
+    for C := 0 to 2 do
+      for K := 0 to 2 do
+        Result[R, C] := Result[R, C] + A[R, K] * B[K, C];
+end;
+
+function RotationMatrix(AX, AY, AZ: Double): TMatrix3x3;
+var
+  rx, ry, rz: Double;
+  cx, sx, cy, sy, cz, sz: Double;
+  Mx, My, Mz, Mtmp: TMatrix3x3;
+begin
+  rx := AX * pi / 180.0;
+  ry := AY * pi / 180.0;
+  rz := AZ * pi / 180.0;
+  
+  cx := Cos(rx); sx := Sin(rx);
+  cy := Cos(ry); sy := Sin(ry);
+  cz := Cos(rz); sz := Sin(rz);
+  
+  Mx := IdentityMatrix;
+  Mx[1, 1] := cx; Mx[1, 2] := -sx;
+  Mx[2, 1] := sx; Mx[2, 2] := cx;
+  
+  My := IdentityMatrix;
+  My[0, 0] := cy; My[0, 2] := sy;
+  My[2, 0] := -sy; My[2, 2] := cy;
+  
+  Mz := IdentityMatrix;
+  Mz[0, 0] := cz; Mz[0, 1] := -sz;
+  Mz[1, 0] := sz; Mz[1, 1] := cz;
+  
+  Mtmp := MultiplyMatrix(Mz, My);
+  Result := MultiplyMatrix(Mtmp, Mx);
+end;
+
+procedure TAISkeletonRig.UpdateFK;
+var
+  I: Integer;
+  ParentIdx: Integer;
+  Rot, LocalRot: TMatrix3x3;
+  OffsetGlobalX, OffsetGlobalY, OffsetGlobalZ: Double;
+begin
+  for I := 0 to Length(FJoints) - 1 do
+  begin
+    ParentIdx := FJoints[I].ParentIndex;
+    LocalRot := RotationMatrix(FJoints[I].AngleX, FJoints[I].AngleY, FJoints[I].AngleZ);
+    
+    if ParentIdx = -1 then
+    begin
+      FJoints[I].GlobalRot := LocalRot;
+      FJoints[I].StartX := 0;
+      FJoints[I].StartY := 0.2; // slight base elevation
+      FJoints[I].StartZ := 0;
+      
+      OffsetGlobalX := LocalRot[0, 0] * FJoints[I].OffsetX + LocalRot[0, 1] * FJoints[I].OffsetY + LocalRot[0, 2] * FJoints[I].OffsetZ;
+      OffsetGlobalY := LocalRot[1, 0] * FJoints[I].OffsetX + LocalRot[1, 1] * FJoints[I].OffsetY + LocalRot[1, 2] * FJoints[I].OffsetZ;
+      OffsetGlobalZ := LocalRot[2, 0] * FJoints[I].OffsetX + LocalRot[2, 1] * FJoints[I].OffsetY + LocalRot[2, 2] * FJoints[I].OffsetZ;
+      
+      FJoints[I].EndX := FJoints[I].StartX + OffsetGlobalX;
+      FJoints[I].EndY := FJoints[I].StartY + OffsetGlobalY;
+      FJoints[I].EndZ := FJoints[I].StartZ + OffsetGlobalZ;
+    end
+    else
+    begin
+      Rot := FJoints[ParentIdx].GlobalRot;
+      FJoints[I].GlobalRot := MultiplyMatrix(Rot, LocalRot);
+      
+      FJoints[I].StartX := FJoints[ParentIdx].EndX;
+      FJoints[I].StartY := FJoints[ParentIdx].EndY;
+      FJoints[I].StartZ := FJoints[ParentIdx].EndZ;
+      
+      // Rotated by parent accumulated global rotation
+      OffsetGlobalX := Rot[0, 0] * FJoints[I].OffsetX + Rot[0, 1] * FJoints[I].OffsetY + Rot[0, 2] * FJoints[I].OffsetZ;
+      OffsetGlobalY := Rot[1, 0] * FJoints[I].OffsetX + Rot[1, 1] * FJoints[I].OffsetY + Rot[1, 2] * FJoints[I].OffsetZ;
+      OffsetGlobalZ := Rot[2, 0] * FJoints[I].OffsetX + Rot[2, 1] * FJoints[I].OffsetY + Rot[2, 2] * FJoints[I].OffsetZ;
+      
+      FJoints[I].EndX := FJoints[I].StartX + OffsetGlobalX;
+      FJoints[I].EndY := FJoints[I].StartY + OffsetGlobalY;
+      FJoints[I].EndZ := FJoints[I].StartZ + OffsetGlobalZ;
+    end;
+  end;
+end;
+
+function TAISkeletonRig.GetJointCount: Integer;
+begin
+  Result := Length(FJoints);
+end;
+
+function TAISkeletonRig.GetJoint(Index: Integer): TBoneJoint;
+begin
+  if (Index >= 0) and (Index < Length(FJoints)) then
+    Result := FJoints[Index]
+  else
+    FillChar(Result, SizeOf(Result), 0);
 end;
 
 end.
---
+++ pacote/samples/AI Graphic/avatar_demo/main.pas
@@ -0,0 +1,509 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  ComCtrls, Math, aiskeletonrig, aiavatarcontroller, aiposelibrary, aibase;
+
+type
+
+  { TfrmAvatarDemo }
+
+  TfrmAvatarDemo = class(TForm)
+    pnlLeft: TPanel;
+    pnlScene: TPanel;
+    lblSceneTitle: TLabel;
+    lblBoneSelect: TLabel;
+    cbBones: TComboBox;
+    lblAxisSelect: TLabel;
+    cbAxis: TComboBox;
+    lblAngle: TLabel;
+    tbAngle: TTrackBar;
+    lblAngleVal: TLabel;
+    
+    pnlPose: TPanel;
+    lblPoseTitle: TLabel;
+    btnTPose: TButton;
+    btnSit: TButton;
+    btnWave: TButton;
+    btnWalk: TButton;
+    
+    pnlView: TPanel;
+    meLogs: TMemo;
+    lblLogs: TLabel;
+
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure cbBonesChange(Sender: TObject);
+    procedure cbAxisChange(Sender: TObject);
+    procedure tbAngleChange(Sender: TObject);
+    procedure btnTPoseClick(Sender: TObject);
+    procedure btnSitClick(Sender: TObject);
+    procedure btnWaveClick(Sender: TObject);
+    procedure btnWalkClick(Sender: TObject);
+    
+    procedure pnlViewPaint(Sender: TObject);
+    procedure pnlViewMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+    procedure pnlViewMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
+    procedure pnlViewMouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+  private
+    FSkeleton: TAISkeletonRig;
+    FAvatar: TAIAvatarController;
+    FPoseLib: TAIPoseLibrary;
+    
+    // View camera angles & zoom
+    FRotX: Double;
+    FRotY: Double;
+    FZoom: Double;
+    FMouseDrag: Boolean;
+    FLastMousePos: TPoint;
+    
+    procedure LogMsg(const AMsg: string);
+    procedure ComponentLog(Sender: TObject; ALevel: TAILogLevel; const AMsg: string);
+    procedure UpdateBoneControls;
+    procedure DrawJoint(ACanvas: TCanvas; const Joint: TBoneJoint; cosX, sinX, cosY, sinY: Double; CX, CY: Integer; Scale, D: Double);
+    procedure DrawBoneLine(ACanvas: TCanvas; const JStart, JEnd: TBoneJoint; cosX, sinX, cosY, sinY: Double; CX, CY: Integer; Scale, D: Double; AColor: TColor; Thickness: Integer);
+  public
+
+  end;
+
+var
+  frmAvatarDemo: TfrmAvatarDemo;
+
+implementation
+
+{$R *.lfm}
+
+{ TfrmAvatarDemo }
+
+procedure TfrmAvatarDemo.FormCreate(Sender: TObject);
+begin
+  // 1. Create AI components
+  FSkeleton := TAISkeletonRig.Create(Self);
+  FAvatar := TAIAvatarController.Create(Self);
+  FAvatar.Skeleton := FSkeleton;
+  FPoseLib := TAIPoseLibrary.Create(Self);
+  
+  // Set logs
+  FSkeleton.OnLog := @ComponentLog;
+  FAvatar.OnLog := @ComponentLog;
+  FPoseLib.OnLog := @ComponentLog;
+
+  // Initialize camera settings
+  FRotX := 15.0;
+  FRotY := -30.0;
+  FZoom := 1.0;
+  FMouseDrag := False;
+
+  // Populate Bone combo
+  cbBones.Items.Assign(FSkeleton.BonesList);
+  cbBones.ItemIndex := 1; // Start on spine
+
+  // Populate Axis combo
+  cbAxis.Items.Clear;
+  cbAxis.Items.Add('Axis X (Pitch)');
+  cbAxis.Items.Add('Axis Y (Yaw)');
+  cbAxis.Items.Add('Axis Z (Roll)');
+  cbAxis.ItemIndex := 2; // Default Z
+
+  UpdateBoneControls;
+  LogMsg('Avatar Crash Test Simulator initialized.');
+  LogMsg('Hint: Drag mouse on the right panel to rotate the camera.');
+end;
+
+procedure TfrmAvatarDemo.FormDestroy(Sender: TObject);
+begin
+  // Freed by Owner (Self)
+end;
+
+procedure TfrmAvatarDemo.ComponentLog(Sender: TObject; ALevel: TAILogLevel; const AMsg: string);
+var
+  Prefix: string;
+begin
+  case ALevel of
+    llDebug: Prefix := '[DEBUG] ';
+    llInfo: Prefix := '[INFO] ';
+    llWarning: Prefix := '[WARNING] ';
+    llError: Prefix := '[ERROR] ';
+  end;
+  LogMsg(Prefix + Sender.ClassName + ': ' + AMsg);
+end;
+
+procedure TfrmAvatarDemo.LogMsg(const AMsg: string);
+begin
+  meLogs.Lines.Append('[' + FormatDateTime('hh:nn:ss.zzz', Now) + '] ' + AMsg);
+end;
+
+procedure TfrmAvatarDemo.UpdateBoneControls;
+var
+  BoneName: string;
+  I: Integer;
+  Joint: TBoneJoint;
+  Angle: Double;
+begin
+  if cbBones.ItemIndex < 0 then Exit;
+  BoneName := cbBones.Text;
+  
+  // Find current angle for selected bone and axis
+  Angle := 0;
+  for I := 0 to FSkeleton.GetJointCount - 1 do
+  begin
+    Joint := FSkeleton.GetJoint(I);
+    if SameText(Joint.Name, BoneName) then
+    begin
+      case cbAxis.ItemIndex of
+        0: Angle := Joint.AngleX;
+        1: Angle := Joint.AngleY;
+        2: Angle := Joint.AngleZ;
+      end;
+      Break;
+    end;
+  end;
+  
+  // Temporarily disable event to avoid recursion
+  tbAngle.OnChange := nil;
+  try
+    tbAngle.Position := Round(Angle);
+    lblAngleVal.Caption := Format('%d°', [Round(Angle)]);
+  finally
+    tbAngle.OnChange := @tbAngleChange;
+  end;
+end;
+
+procedure TfrmAvatarDemo.cbBonesChange(Sender: TObject);
+begin
+  UpdateBoneControls;
+end;
+
+procedure TfrmAvatarDemo.cbAxisChange(Sender: TObject);
+begin
+  UpdateBoneControls;
+end;
+
+procedure TfrmAvatarDemo.tbAngleChange(Sender: TObject);
+var
+  BoneName: string;
+  I: Integer;
+  Joint: TBoneJoint;
+  AX, AY, AZ: Double;
+begin
+  if cbBones.ItemIndex < 0 then Exit;
+  BoneName := cbBones.Text;
+  
+  AX := 0; AY := 0; AZ := 0;
+  
+  // Find current angles
+  for I := 0 to FSkeleton.GetJointCount - 1 do
+  begin
+    Joint := FSkeleton.GetJoint(I);
+    if SameText(Joint.Name, BoneName) then
+    begin
+      AX := Joint.AngleX;
+      AY := Joint.AngleY;
+      AZ := Joint.AngleZ;
+      Break;
+    end;
+  end;
+  
+  // Update selected axis
+  case cbAxis.ItemIndex of
+    0: AX := tbAngle.Position;
+    1: AY := tbAngle.Position;
+    2: AZ := tbAngle.Position;
+  end;
+  
+  lblAngleVal.Caption := Format('%d°', [tbAngle.Position]);
+  
+  // Apply rotation
+  FSkeleton.SetBoneRotation(BoneName, AX, AY, AZ);
+  pnlView.Invalidate;
+end;
+
+procedure TfrmAvatarDemo.btnTPoseClick(Sender: TObject);
+var
+  I: Integer;
+begin
+  LogMsg('Applying T-Pose...');
+  FAvatar.SetPose('T-Pose');
+  for I := 0 to FSkeleton.GetJointCount - 1 do
+    FSkeleton.SetBoneRotation(FSkeleton.GetJoint(I).Name, 0, 0, 0);
+  UpdateBoneControls;
+  pnlView.Invalidate;
+end;
+
+procedure TfrmAvatarDemo.btnSitClick(Sender: TObject);
+begin
+  LogMsg('Applying sitting pose...');
+  FAvatar.SetPose('Sitting');
+  
+  // Reset all
+  btnTPoseClick(nil);
+  
+  // Left hip pitch 90, Right hip pitch 90
+  FSkeleton.SetBoneRotation('left_hip', 90, 0, 0);
+  FSkeleton.SetBoneRotation('right_hip', 90, 0, 0);
+  
+  // Left knee pitch -90, Right knee pitch -90
+  FSkeleton.SetBoneRotation('left_knee', -90, 0, 0);
+  FSkeleton.SetBoneRotation('right_knee', -90, 0, 0);
+  
+  // Arms slightly forward
+  FSkeleton.SetBoneRotation('left_shoulder', 0, 0, -30);
+  FSkeleton.SetBoneRotation('right_shoulder', 0, 0, 30);
+  
+  UpdateBoneControls;
+  pnlView.Invalidate;
+end;
+
+procedure TfrmAvatarDemo.btnWaveClick(Sender: TObject);
+begin
+  LogMsg('Applying waving pose...');
+  FAvatar.SetPose('Waving');
+  
+  btnTPoseClick(nil);
+  
+  // Raise right shoulder high up (130 degrees roll)
+  FSkeleton.SetBoneRotation('right_shoulder', 0, 0, 130);
+  // Bend right elbow (90 degrees roll)
+  FSkeleton.SetBoneRotation('right_elbow', 0, 0, 90);
+  
+  UpdateBoneControls;
+  pnlView.Invalidate;
+end;
+
+procedure TfrmAvatarDemo.btnWalkClick(Sender: TObject);
+begin
+  LogMsg('Applying walking stance...');
+  FAvatar.SetPose('Walking');
+  
+  btnTPoseClick(nil);
+  
+  // Opposing leg angles
+  FSkeleton.SetBoneRotation('left_hip', 30, 0, 0);
+  FSkeleton.SetBoneRotation('left_knee', -20, 0, 0);
+  
+  FSkeleton.SetBoneRotation('right_hip', -30, 0, 0);
+  FSkeleton.SetBoneRotation('right_knee', -5, 0, 0);
+  
+  // Opposing arm angles
+  FSkeleton.SetBoneRotation('left_shoulder', -25, 0, -10);
+  FSkeleton.SetBoneRotation('right_shoulder', 25, 0, 10);
+  FSkeleton.SetBoneRotation('right_elbow', 0, 0, 30);
+  
+  UpdateBoneControls;
+  pnlView.Invalidate;
+end;
+
+procedure TfrmAvatarDemo.DrawJoint(ACanvas: TCanvas; const Joint: TBoneJoint; cosX, sinX, cosY, sinY: Double; CX, CY: Integer; Scale, D: Double);
+var
+  x1, y1, z1, x2, y2, z2: Double;
+  sx, sy: Integer;
+  R: Integer;
+begin
+  // Project end point of joint
+  x1 := Joint.EndX * cosY - Joint.EndZ * sinY;
+  z1 := Joint.EndX * sinY + Joint.EndZ * cosY;
+  y1 := Joint.EndY;
+  
+  y2 := y1 * cosX - z1 * sinX;
+  z2 := y1 * sinX + z1 * cosX;
+  x2 := x1;
+  
+  sx := CX + Round(x2 * Scale * D / (z2 + D));
+  sy := CY - Round(y2 * Scale * D / (z2 + D));
+  
+  R := Max(2, Round(8 * D / (z2 + D)));
+  
+  if SameText(Joint.Name, 'head') then
+  begin
+    // Head oval
+    ACanvas.Brush.Color := RGBToColor(255, 210, 120);
+    ACanvas.Pen.Color := RGBToColor(200, 150, 80);
+    ACanvas.Ellipse(sx - R * 2, sy - R * 3, sx + R * 2, sy + R * 1);
+    
+    // Draw face cross or dummy crash test marker!
+    ACanvas.Pen.Color := clBlack;
+    ACanvas.Line(sx - R * 2, sy - R, sx + R * 2, sy - R);
+    ACanvas.Line(sx, sy - R * 3, sx, sy + R * 1);
+  end
+  else
+  begin
+    // General joint marker
+    ACanvas.Brush.Color := clYellow;
+    ACanvas.Pen.Color := RGBToColor(255, 127, 0);
+    ACanvas.Ellipse(sx - R, sy - R, sx + R, sy + R);
+  end;
+end;
+
+procedure TfrmAvatarDemo.DrawBoneLine(ACanvas: TCanvas; const JStart, JEnd: TBoneJoint; cosX, sinX, cosY, sinY: Double; CX, CY: Integer; Scale, D: Double; AColor: TColor; Thickness: Integer);
+var
+  x1, y1, z1, x2, y2, z2: Double;
+  sx1, sy1, sx2, sy2: Integer;
+begin
+  // Start Point Project
+  x1 := JStart.EndX * cosY - JStart.EndZ * sinY;
+  z1 := JStart.EndX * sinY + JStart.EndZ * cosY;
+  y1 := JStart.EndY;
+  y2 := y1 * cosX - z1 * sinX;
+  z2 := y1 * sinX + z1 * cosX;
+  x2 := x1;
+  sx1 := CX + Round(x2 * Scale * D / (z2 + D));
+  sy1 := CY - Round(y2 * Scale * D / (z2 + D));
+  
+  // End Point Project
+  x1 := JEnd.EndX * cosY - JEnd.EndZ * sinY;
+  z1 := JEnd.EndX * sinY + JEnd.EndZ * cosY;
+  y1 := JEnd.EndY;
+  y2 := y1 * cosX - z1 * sinX;
+  z2 := y1 * sinX + z1 * cosX;
+  x2 := x1;
+  sx2 := CX + Round(x2 * Scale * D / (z2 + D));
+  sy2 := CY - Round(y2 * Scale * D / (z2 + D));
+  
+  ACanvas.Pen.Color := AColor;
+  ACanvas.Pen.Width := Thickness;
+  ACanvas.Line(sx1, sy1, sx2, sy2);
+  ACanvas.Pen.Width := 1; // restore
+end;
+
+procedure TfrmAvatarDemo.pnlViewPaint(Sender: TObject);
+var
+  CanvasLocal: TCanvas;
+  CX, CY: Integer;
+  BaseScale, Scale: Double;
+  D: Double;
+  radX, radY: Double;
+  cosX, sinX, cosY, sinY: Double;
+  I: Integer;
+  Joint: TBoneJoint;
+  
+  // Floor grid points
+  GX, GZ: Double;
+  sx, sy: Integer;
+  x1, z1, y2, z2, x2: Double;
+begin
+  CanvasLocal := pnlView.Canvas;
+  CanvasLocal.Brush.Color := clWhite;
+  CanvasLocal.FillRect(pnlView.ClientRect);
+  
+  CX := pnlView.ClientWidth div 2;
+  CY := pnlView.ClientHeight div 2 - 30; // Shift origin slightly up to see floor better
+  
+  BaseScale := Min(pnlView.ClientWidth, pnlView.ClientHeight) * 0.35;
+  Scale := BaseScale * FZoom;
+  D := 5.0; // Camera distance
+  
+  radY := FRotY * pi / 180.0;
+  radX := FRotX * pi / 180.0;
+  cosY := Cos(radY); sinY := Sin(radY);
+  cosX := Cos(radX); sinX := Sin(radX);
+  
+  // 1. Draw floor grid for depth perspective
+  CanvasLocal.Pen.Color := RGBToColor(220, 220, 220);
+  for I := -5 to 5 do
+  begin
+    // Grid lines parallel to Z axis (from left to right)
+    GX := I * 0.3;
+    x1 := GX * cosY - (-1.5) * sinY;
+    z1 := GX * sinY + (-1.5) * cosY;
+    y2 := 0.0 * cosX - z1 * sinX;
+    z2 := 0.0 * sinX + z1 * cosX;
+    x2 := x1;
+    sx := CX + Round(x2 * Scale * D / (z2 + D));
+    sy := CY - Round(y2 * Scale * D / (z2 + D));
+    CanvasLocal.MoveTo(sx, sy);
+    
+    x1 := GX * cosY - 1.5 * sinY;
+    z1 := GX * sinY + 1.5 * cosY;
+    y2 := 0.0 * cosX - z1 * sinX;
+    z2 := 0.0 * sinX + z1 * cosX;
+    x2 := x1;
+    sx := CX + Round(x2 * Scale * D / (z2 + D));
+    sy := CY - Round(y2 * Scale * D / (z2 + D));
+    CanvasLocal.LineTo(sx, sy);
+    
+    // Grid lines parallel to X axis (from back to front)
+    GZ := I * 0.3;
+    x1 := (-1.5) * cosY - GZ * sinY;
+    z1 := (-1.5) * sinY + GZ * cosY;
+    y2 := 0.0 * cosX - z1 * sinX;
+    z2 := 0.0 * sinX + z1 * cosX;
+    x2 := x1;
+    sx := CX + Round(x2 * Scale * D / (z2 + D));
+    sy := CY - Round(y2 * Scale * D / (z2 + D));
+    CanvasLocal.MoveTo(sx, sy);
+    
+    x1 := 1.5 * cosY - GZ * sinY;
+    z1 := 1.5 * sinY + GZ * cosY;
+    y2 := 0.0 * cosX - z1 * sinX;
+    z2 := 0.0 * sinX + z1 * cosX;
+    x2 := x1;
+    sx := CX + Round(x2 * Scale * D / (z2 + D));
+    sy := CY - Round(y2 * Scale * D / (z2 + D));
+    CanvasLocal.LineTo(sx, sy);
+  end;
+
+  // 2. Draw skeleton bones hierarchically
+  DrawBoneLine(CanvasLocal, FSkeleton.GetJoint(0), FSkeleton.GetJoint(1), cosX, sinX, cosY, sinY, CX, CY, Scale, D, clBlack, 4); // pelvis to spine
+  DrawBoneLine(CanvasLocal, FSkeleton.GetJoint(1), FSkeleton.GetJoint(2), cosX, sinX, cosY, sinY, CX, CY, Scale, D, clBlack, 4); // spine to head
+  
+  // Left arm
+  DrawBoneLine(CanvasLocal, FSkeleton.GetJoint(1), FSkeleton.GetJoint(3), cosX, sinX, cosY, sinY, CX, CY, Scale, D, clRed, 3);
+  DrawBoneLine(CanvasLocal, FSkeleton.GetJoint(3), FSkeleton.GetJoint(4), cosX, sinX, cosY, sinY, CX, CY, Scale, D, clRed, 3);
+  
+  // Right arm
+  DrawBoneLine(CanvasLocal, FSkeleton.GetJoint(1), FSkeleton.GetJoint(5), cosX, sinX, cosY, sinY, CX, CY, Scale, D, clGreen, 3);
+  DrawBoneLine(CanvasLocal, FSkeleton.GetJoint(5), FSkeleton.GetJoint(6), cosX, sinX, cosY, sinY, CX, CY, Scale, D, clGreen, 3);
+  
+  // Left leg
+  DrawBoneLine(CanvasLocal, FSkeleton.GetJoint(0), FSkeleton.GetJoint(7), cosX, sinX, cosY, sinY, CX, CY, Scale, D, clRed, 3);
+  DrawBoneLine(CanvasLocal, FSkeleton.GetJoint(7), FSkeleton.GetJoint(8), cosX, sinX, cosY, sinY, CX, CY, Scale, D, clRed, 3);
+  
+  // Right leg
+  DrawBoneLine(CanvasLocal, FSkeleton.GetJoint(0), FSkeleton.GetJoint(9), cosX, sinX, cosY, sinY, CX, CY, Scale, D, clGreen, 3);
+  DrawBoneLine(CanvasLocal, FSkeleton.GetJoint(9), FSkeleton.GetJoint(10), cosX, sinX, cosY, sinY, CX, CY, Scale, D, clGreen, 3);
+
+  // 3. Draw joint dots
+  for I := 0 to FSkeleton.GetJointCount - 1 do
+  begin
+    Joint := FSkeleton.GetJoint(I);
+    DrawJoint(CanvasLocal, Joint, cosX, sinX, cosY, sinY, CX, CY, Scale, D);
+  end;
+  
+  // Draw Coordinate axes at corner
+  CanvasLocal.Pen.Color := clRed; CanvasLocal.Line(10, pnlView.Height - 10, 40, pnlView.Height - 10); // X
+  CanvasLocal.TextOut(42, pnlView.Height - 18, 'X');
+  CanvasLocal.Pen.Color := clGreen; CanvasLocal.Line(10, pnlView.Height - 10, 10, pnlView.Height - 40); // Y
+  CanvasLocal.TextOut(6, pnlView.Height - 52, 'Y');
+end;
+
+procedure TfrmAvatarDemo.pnlViewMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+begin
+  if Button = mbLeft then
+  begin
+    FMouseDrag := True;
+    FLastMousePos := Point(X, Y);
+  end;
+end;
+
+procedure TfrmAvatarDemo.pnlViewMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
+begin
+  if FMouseDrag then
+  begin
+    FRotY := FRotY + (X - FLastMousePos.X) * 0.5;
+    FRotX := FRotX + (Y - FLastMousePos.Y) * 0.5;
+    FLastMousePos := Point(X, Y);
+    pnlView.Invalidate;
+  end;
+end;
+
+procedure TfrmAvatarDemo.pnlViewMouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+begin
+  if Button = mbLeft then
+    FMouseDrag := False;
+end;
+
+end.
