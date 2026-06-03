9534475dab13a08c46f65c5fd7796b84a7c005c4
Marcelo Maurin <Mmaurin@saude.lan>
2026-06-03 16:16:49 -0300

    feat: implement 3D perspective projection, rendering, flat shading, mouse rotation, and mouse wheel zoom in TAI3DModelViewer

--- pacote/AI Graphic/ai3dmodelviewer.pas
+++ pacote/AI Graphic/ai3dmodelviewer.pas
@@ -5,7 +5,7 @@ unit ai3dmodelviewer;
 interface
 
 uses
-  Classes, SysUtils, Controls, Graphics, aimodel3d, aibase;
+  Classes, SysUtils, Controls, Graphics, Math, aimodel3d, aibase;
 
 type
   TRenderMode = (rmSolid, rmWireframe, rmPoints);
@@ -19,6 +19,14 @@ type
     FBackgroundColor: TColor;
     FOnModelLoaded: TNotifyEvent;
     FOnModelError: TNotifyEvent;
+    
+    // Rotation & Zoom fields
+    FRotX: Double;
+    FRotY: Double;
+    FRotZ: Double;
+    FZoom: Double;
+    FMouseDrag: Boolean;
+    FLastMousePos: TPoint;
     
     // AI Suite common fields
     FPrompt: string;
@@ -34,6 +42,10 @@ type
     procedure Log(ALevel: TAILogLevel; const AMessage: string);
   protected
     procedure Notification(AComponent: TComponent; Operation: TOperation); override;
+    procedure MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Integer); override;
+    procedure MouseMove(Shift: TShiftState; X, Y: Integer); override;
+    procedure MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: Integer); override;
+    function DoMouseWheel(Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint): Boolean; override;
   public
     constructor Create(AOwner: TComponent); override;
     procedure Paint; override;
@@ -79,6 +91,12 @@ begin
   FLastError := '';
   FLastResult := '';
   FLastSuccess := True;
+  
+  FRotX := 20.0;
+  FRotY := 30.0;
+  FRotZ := 0.0;
+  FZoom := 1.0;
+  FMouseDrag := False;
 end;
 
 procedure TAI3DModelViewer.ClearError;
@@ -114,7 +132,7 @@ begin
       if Assigned(FOnModelLoaded) then
         FOnModelLoaded(Self);
     end;
-    Invalidate;
+    ResetCamera;
   end;
 end;
 
@@ -125,37 +143,229 @@ begin
     FModel := nil;
 end;
 
+procedure TAI3DModelViewer.MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+begin
+  inherited MouseDown(Button, Shift, X, Y);
+  if Button = mbLeft then
+  begin
+    FMouseDrag := True;
+    FLastMousePos := Point(X, Y);
+    SetFocus;
+  end;
+end;
+
+procedure TAI3DModelViewer.MouseMove(Shift: TShiftState; X, Y: Integer);
+begin
+  inherited MouseMove(Shift, X, Y);
+  if FMouseDrag then
+  begin
+    FRotY := FRotY + (X - FLastMousePos.X) * 0.5;
+    FRotX := FRotX + (Y - FLastMousePos.Y) * 0.5;
+    FLastMousePos := Point(X, Y);
+    Invalidate;
+  end;
+end;
+
+procedure TAI3DModelViewer.MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+begin
+  inherited MouseUp(Button, Shift, X, Y);
+  if Button = mbLeft then
+    FMouseDrag := False;
+end;
+
+function TAI3DModelViewer.DoMouseWheel(Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint): Boolean;
+begin
+  Result := True;
+  if WheelDelta > 0 then
+    ZoomIn
+  else
+    ZoomOut;
+end;
+
+type
+  TProjectedFace = record
+    P1, P2, P3: TPoint;
+    AvgZ: Single;
+    Intensity: Single;
+  end;
+
 procedure TAI3DModelViewer.Paint;
+var
+  BaseScale: Double;
+  ProjectedFaces: array of TProjectedFace;
+  I: Integer;
+  radX, radY: Double;
+  cosX, sinX, cosY, sinY: Double;
+  CX, CY: Integer;
+  D: Double;
+  rz1, rz2, rz3: Single;
+  
+  procedure RotateAndProject(const V: TVertex3D; var P: TPoint; var RZ: Single);
+  var
+    tx, ty, tz: Single;
+    x1, y1, z1: Single;
+    x2, y2, z2: Single;
+    Scale: Double;
+  begin
+    tx := V.X - FModel.MidX;
+    ty := V.Y - FModel.MidY;
+    tz := V.Z - FModel.MidZ;
+    
+    // Rotate Y
+    x1 := tx * cosY - tz * sinY;
+    z1 := tx * sinY + tz * cosY;
+    y1 := ty;
+    
+    // Rotate X
+    y2 := y1 * cosX - z1 * sinX;
+    z2 := y1 * sinX + z1 * cosX;
+    x2 := x1;
+    
+    RZ := z2;
+    
+    // Project to 2D
+    Scale := BaseScale * FZoom;
+    P.X := CX + Round(x2 * Scale * D / (z2 + D));
+    P.Y := CY - Round(y2 * Scale * D / (z2 + D));
+  end;
+
+  procedure QuickSort(L, R: Integer);
+  var
+    IdxI, IdxJ: Integer;
+    Pivot: Single;
+    Temp: TProjectedFace;
+  begin
+    IdxI := L;
+    IdxJ := R;
+    Pivot := ProjectedFaces[(L + R) div 2].AvgZ;
+    repeat
+      while ProjectedFaces[IdxI].AvgZ > Pivot do Inc(IdxI);
+      while ProjectedFaces[IdxJ].AvgZ < Pivot do Dec(IdxJ);
+      if IdxI <= IdxJ then
+      begin
+        Temp := ProjectedFaces[IdxI];
+        ProjectedFaces[IdxI] := ProjectedFaces[IdxJ];
+        ProjectedFaces[IdxJ] := Temp;
+        Inc(IdxI);
+        Dec(IdxJ);
+      end;
+    until IdxI > IdxJ;
+    if L < IdxJ then QuickSort(L, IdxJ);
+    if IdxI < R then QuickSort(IdxI, R);
+  end;
+
+var
+  R, G, B: Byte;
+  Intensity: Single;
 begin
   inherited Paint;
   Canvas.Brush.Color := FBackgroundColor;
   Canvas.FillRect(ClientRect);
   
   Canvas.Pen.Color := clBlack;
+  Canvas.Brush.Style := bsClear;
   Canvas.TextOut(10, 10, '3D Model Viewer');
-  if Assigned(FModel) then
+  
+  if (FModel = nil) or (Length(FModel.Faces) = 0) then
   begin
-    Canvas.TextOut(10, 30, 'Model: ' + FModel.FilePath);
-    Canvas.TextOut(10, 50, Format('Vertices: %d, Faces: %d', [FModel.VerticesCount, FModel.FacesCount]));
-  end
-  else
-    Canvas.TextOut(10, 30, 'No Model Loaded');
+    Canvas.TextOut(10, 30, 'No Model Loaded (or empty mesh)');
+    Exit;
+  end;
+
+  Canvas.TextOut(10, 30, 'Model: ' + ExtractFileName(FModel.FilePath));
+  Canvas.TextOut(10, 50, Format('Vertices: %d, Faces: %d', [FModel.VerticesCount, FModel.FacesCount]));
+  Canvas.TextOut(10, 70, 'Hint: Drag mouse to Rotate. Mouse wheel / Zoom buttons to Zoom.');
+
+  // Base Scale calculation
+  BaseScale := (Min(ClientWidth, ClientHeight) * 0.45) / FModel.ModelRadius;
+  if BaseScale < 0.1 then BaseScale := 1.0;
+  
+  CX := ClientWidth div 2;
+  CY := ClientHeight div 2;
+  D := FModel.ModelRadius * 3.0;
+  if D < 1.0 then D := 10.0;
+  
+  radY := FRotY * pi / 180.0;
+  radX := FRotX * pi / 180.0;
+  cosY := Cos(radY); sinY := Sin(radY);
+  cosX := Cos(radX); sinX := Sin(radX);
+
+  SetLength(ProjectedFaces, Length(FModel.Faces));
+  for I := 0 to Length(FModel.Faces) - 1 do
+  begin
+    RotateAndProject(FModel.Faces[I].V1, ProjectedFaces[I].P1, rz1);
+    RotateAndProject(FModel.Faces[I].V2, ProjectedFaces[I].P2, rz2);
+    RotateAndProject(FModel.Faces[I].V3, ProjectedFaces[I].P3, rz3);
+    ProjectedFaces[I].AvgZ := (rz1 + rz2 + rz3) / 3.0;
+
+    // Normal calculation for simple light shading
+    rz1 := FModel.Faces[I].Normal.X * cosY - FModel.Faces[I].Normal.Z * sinY;
+    rz2 := FModel.Faces[I].Normal.X * sinY + FModel.Faces[I].Normal.Z * cosY;
+    rz3 := FModel.Faces[I].Normal.Y * cosX - rz2 * sinX;
+    
+    Intensity := Abs(rz3);
+    if Intensity < 0.1 then Intensity := 0.1;
+    ProjectedFaces[I].Intensity := Intensity;
+  end;
+
+  // Sort faces back to front (Painter's algorithm)
+  if Length(ProjectedFaces) > 1 then
+    QuickSort(0, Length(ProjectedFaces) - 1);
+
+  // Draw faces
+  for I := 0 to Length(ProjectedFaces) - 1 do
+  begin
+    case FRenderMode of
+      rmSolid:
+        begin
+          Intensity := ProjectedFaces[I].Intensity;
+          R := Round(40 + 160 * Intensity);
+          G := Round(60 + 175 * Intensity);
+          B := Round(90 + 195 * Intensity);
+          Canvas.Brush.Color := RGBToColor(R, G, B);
+          Canvas.Brush.Style := bsSolid;
+          Canvas.Pen.Color := RGBToColor(Max(0, R - 30), Max(0, G - 30), Max(0, B - 30));
+          Canvas.Polygon([ProjectedFaces[I].P1, ProjectedFaces[I].P2, ProjectedFaces[I].P3]);
+        end;
+      rmWireframe:
+        begin
+          Canvas.Brush.Style := bsClear;
+          Canvas.Pen.Color := clNavy;
+          Canvas.Polygon([ProjectedFaces[I].P1, ProjectedFaces[I].P2, ProjectedFaces[I].P3]);
+        end;
+      rmPoints:
+        begin
+          Canvas.Pen.Color := clNavy;
+          Canvas.Brush.Color := clNavy;
+          Canvas.Brush.Style := bsSolid;
+          Canvas.Ellipse(ProjectedFaces[I].P1.X - 1, ProjectedFaces[I].P1.Y - 1, ProjectedFaces[I].P1.X + 1, ProjectedFaces[I].P1.Y + 1);
+          Canvas.Ellipse(ProjectedFaces[I].P2.X - 1, ProjectedFaces[I].P2.Y - 1, ProjectedFaces[I].P2.X + 1, ProjectedFaces[I].P2.Y + 1);
+          Canvas.Ellipse(ProjectedFaces[I].P3.X - 1, ProjectedFaces[I].P3.Y - 1, ProjectedFaces[I].P3.X + 1, ProjectedFaces[I].P3.Y + 1);
+        end;
+    end;
+  end;
 end;
 
 procedure TAI3DModelViewer.ZoomIn;
 begin
+  FZoom := FZoom * 1.15;
   Log(llDebug, 'Zoomed in.');
   Invalidate;
 end;
 
 procedure TAI3DModelViewer.ZoomOut;
 begin
+  FZoom := FZoom / 1.15;
   Log(llDebug, 'Zoomed out.');
   Invalidate;
 end;
 
 procedure TAI3DModelViewer.ResetCamera;
 begin
+  FRotX := 20.0;
+  FRotY := 30.0;
+  FRotZ := 0.0;
+  FZoom := 1.0;
   Log(llDebug, 'Camera reset.');
   Invalidate;
 end;
--- pacote/AI Graphic/aimodel3d.pas
+++ pacote/AI Graphic/aimodel3d.pas
@@ -8,6 +8,17 @@ uses
   Classes, SysUtils, StrUtils, aibase;
 
 type
+  TVertex3D = record
+    X, Y, Z: Single;
+  end;
+
+  TFace3D = record
+    V1, V2, V3: TVertex3D;
+    Normal: TVertex3D;
+  end;
+
+  TFaceArray = array of TFace3D;
+
   { TAIModel3D }
 
   TAIModel3D = class(TAIBaseComponent)
@@ -15,11 +26,27 @@ type
     FVerticesCount: Integer;
     FFacesCount: Integer;
     FFilePath: string;
+    FFaces: TFaceArray;
+    FMinX, FMaxX, FMinY, FMaxY, FMinZ, FMaxZ: Single;
+    FMidX, FMidY, FMidZ: Single;
+    FModelRadius: Single;
   public
     constructor Create(AOwner: TComponent); override;
     procedure LoadFromFile(const AFileName: string);
     procedure SaveToFile(const AFileName: string);
     procedure Rotate(const AX, AY, AZ: Double);
+    
+    property Faces: TFaceArray read FFaces;
+    property MinX: Single read FMinX;
+    property MaxX: Single read FMaxX;
+    property MinY: Single read FMinY;
+    property MaxY: Single read FMaxY;
+    property MinZ: Single read FMinZ;
+    property MaxZ: Single read FMaxZ;
+    property MidX: Single read FMidX;
+    property MidY: Single read FMidY;
+    property MidZ: Single read FMidZ;
+    property ModelRadius: Single read FModelRadius;
   published
     property VerticesCount: Integer read FVerticesCount write FVerticesCount;
     property FacesCount: Integer read FFacesCount write FFacesCount;
@@ -45,6 +72,10 @@ begin
   FVerticesCount := 0;
   FFacesCount := 0;
   FFilePath := '';
+  SetLength(FFaces, 0);
+  FMinX := 0; FMaxX := 0; FMinY := 0; FMaxY := 0; FMinZ := 0; FMaxZ := 0;
+  FMidX := 0; FMidY := 0; FMidZ := 0;
+  FModelRadius := 1.0;
   ClearError;
 end;
 
@@ -58,10 +89,73 @@ var
   I: Integer;
   Line: string;
   VCount, FCount: Integer;
+  Face: TFace3D;
+  VIdx: Integer;
+  Tokens: TStringList;
+  
+  function ParseFloat(const S: string): Single;
+  var
+    FS: TFormatSettings;
+  begin
+    FS := DefaultFormatSettings;
+    if Pos('.', S) > 0 then
+      FS.DecimalSeparator := '.'
+    else if Pos(',', S) > 0 then
+      FS.DecimalSeparator := ',';
+    Result := StrToFloatDef(Trim(S), 0.0, FS);
+  end;
+
+  procedure CalcBoundingBox;
+  var
+    Idx: Integer;
+    DX, DY, DZ: Single;
+  begin
+    if Length(FFaces) = 0 then Exit;
+    FMinX := FFaces[0].V1.X; FMaxX := FMinX;
+    FMinY := FFaces[0].V1.Y; FMaxY := FMinY;
+    FMinZ := FFaces[0].V1.Z; FMaxZ := FMinZ;
+    for Idx := 0 to Length(FFaces) - 1 do
+    begin
+      // V1
+      if FFaces[Idx].V1.X < FMinX then FMinX := FFaces[Idx].V1.X;
+      if FFaces[Idx].V1.X > FMaxX then FMaxX := FFaces[Idx].V1.X;
+      if FFaces[Idx].V1.Y < FMinY then FMinY := FFaces[Idx].V1.Y;
+      if FFaces[Idx].V1.Y > FMaxY then FMaxY := FFaces[Idx].V1.Y;
+      if FFaces[Idx].V1.Z < FMinZ then FMinZ := FFaces[Idx].V1.Z;
+      if FFaces[Idx].V1.Z > FMaxZ then FMaxZ := FFaces[Idx].V1.Z;
+      // V2
+      if FFaces[Idx].V2.X < FMinX then FMinX := FFaces[Idx].V2.X;
+      if FFaces[Idx].V2.X > FMaxX then FMaxX := FFaces[Idx].V2.X;
+      if FFaces[Idx].V2.Y < FMinY then FMinY := FFaces[Idx].V2.Y;
+      if FFaces[Idx].V2.Y > FMaxY then FMaxY := FFaces[Idx].V2.Y;
+      if FFaces[Idx].V2.Z < FMinZ then FMinZ := FFaces[Idx].V2.Z;
+      if FFaces[Idx].V2.Z > FMaxZ then FMaxZ := FFaces[Idx].V2.Z;
+      // V3
+      if FFaces[Idx].V3.X < FMinX then FMinX := FFaces[Idx].V3.X;
+      if FFaces[Idx].V3.X > FMaxX then FMaxX := FFaces[Idx].V3.X;
+      if FFaces[Idx].V3.Y < FMinY then FMinY := FFaces[Idx].V3.Y;
+      if FFaces[Idx].V3.Y > FMaxY then FMaxY := FFaces[Idx].V3.Y;
+      if FFaces[Idx].V3.Z < FMinZ then FMinZ := FFaces[Idx].V3.Z;
+      if FFaces[Idx].V3.Z > FMaxZ then FMaxZ := FFaces[Idx].V3.Z;
+    end;
+    FMidX := (FMinX + FMaxX) / 2.0;
+    FMidY := (FMinY + FMaxY) / 2.0;
+    FMidZ := (FMinZ + FMaxZ) / 2.0;
+    DX := FMaxX - FMinX;
+    DY := FMaxY - FMinY;
+    DZ := FMaxZ - FMinZ;
+    FModelRadius := Sqrt(DX*DX + DY*DY + DZ*DZ) / 2.0;
+    if FModelRadius < 0.1 then FModelRadius := 1.0;
+  end;
+
 begin
   FFilePath := AFileName;
   Log(llInfo, 'Loading 3D model from: ' + AFileName);
   ClearError;
+  SetLength(FFaces, 0);
+  FMinX := 0; FMaxX := 0; FMinY := 0; FMaxY := 0; FMinZ := 0; FMaxZ := 0;
+  FMidX := 0; FMidY := 0; FMidZ := 0;
+  FModelRadius := 1.0;
 
   if not FileExists(AFileName) then
   begin
@@ -82,7 +176,6 @@ begin
       begin
         FStream.ReadBuffer(Header, 80);
         FStream.ReadBuffer(TrianglesCount, 4);
-        // A binary STL file size must match exactly: 80 + 4 + N * 50
         if FStream.Size = Int64(84) + Int64(TrianglesCount) * 50 then
           IsBinary := True;
       end;
@@ -91,6 +184,24 @@ begin
       begin
         FFacesCount := TrianglesCount;
         FVerticesCount := TrianglesCount * 3;
+        SetLength(FFaces, TrianglesCount);
+        for I := 0 to TrianglesCount - 1 do
+        begin
+          FStream.ReadBuffer(Face.Normal.X, 4);
+          FStream.ReadBuffer(Face.Normal.Y, 4);
+          FStream.ReadBuffer(Face.Normal.Z, 4);
+          FStream.ReadBuffer(Face.V1.X, 4);
+          FStream.ReadBuffer(Face.V1.Y, 4);
+          FStream.ReadBuffer(Face.V1.Z, 4);
+          FStream.ReadBuffer(Face.V2.X, 4);
+          FStream.ReadBuffer(Face.V2.Y, 4);
+          FStream.ReadBuffer(Face.V2.Z, 4);
+          FStream.ReadBuffer(Face.V3.X, 4);
+          FStream.ReadBuffer(Face.V3.Y, 4);
+          FStream.ReadBuffer(Face.V3.Z, 4);
+          FStream.Seek(2, soFromCurrent);
+          FFaces[I] := Face;
+        end;
         FLastResult := Format('Binary STL loaded. Vertices: %d, Faces: %d', [FVerticesCount, FFacesCount]);
         FLastSuccess := True;
         Log(llInfo, FLastResult);
@@ -103,10 +214,14 @@ begin
     if not IsBinary then
     begin
       Lines := TStringList.Create;
+      Tokens := TStringList.Create;
+      Tokens.Delimiter := ' ';
+      Tokens.StrictDelimiter := False;
       try
         Lines.LoadFromFile(AFileName);
         VCount := 0;
         FCount := 0;
+        
         for I := 0 to Lines.Count - 1 do
         begin
           Line := Trim(Lines[I]);
@@ -115,20 +230,72 @@ begin
           if StartsText('vertex', Line) or StartsText('vertex ', Line) then
             Inc(VCount);
         end;
-        FVerticesCount := VCount;
+        
         FFacesCount := FCount;
+        FVerticesCount := VCount;
+        SetLength(FFaces, FCount);
+        
+        FCount := 0;
+        VIdx := 0;
+        for I := 0 to Lines.Count - 1 do
+        begin
+          Tokens.DelimitedText := Trim(Lines[I]);
+          if Tokens.Count = 0 then Continue;
+          
+          if (Tokens[0] = 'facet') and (Tokens.Count >= 5) and (Tokens[1] = 'normal') then
+          begin
+            Face.Normal.X := ParseFloat(Tokens[2]);
+            Face.Normal.Y := ParseFloat(Tokens[3]);
+            Face.Normal.Z := ParseFloat(Tokens[4]);
+            VIdx := 0;
+          end
+          else if (Tokens[0] = 'vertex') and (Tokens.Count >= 4) then
+          begin
+            if VIdx = 0 then
+            begin
+              Face.V1.X := ParseFloat(Tokens[1]);
+              Face.V1.Y := ParseFloat(Tokens[2]);
+              Face.V1.Z := ParseFloat(Tokens[3]);
+              Inc(VIdx);
+            end
+            else if VIdx = 1 then
+            begin
+              Face.V2.X := ParseFloat(Tokens[1]);
+              Face.V2.Y := ParseFloat(Tokens[2]);
+              Face.V2.Z := ParseFloat(Tokens[3]);
+              Inc(VIdx);
+            end
+            else if VIdx = 2 then
+            begin
+              Face.V3.X := ParseFloat(Tokens[1]);
+              Face.V3.Y := ParseFloat(Tokens[2]);
+              Face.V3.Z := ParseFloat(Tokens[3]);
+              Inc(VIdx);
+              if FCount < Length(FFaces) then
+              begin
+                FFaces[FCount] := Face;
+                Inc(FCount);
+              end;
+            end;
+          end;
+        end;
         FLastResult := Format('ASCII STL loaded. Vertices: %d, Faces: %d', [VCount, FCount]);
         FLastSuccess := True;
         Log(llInfo, FLastResult);
       finally
+        Tokens.Free;
         Lines.Free;
       end;
     end;
+    
+    CalcBoundingBox;
+    
   except
     on E: Exception do
     begin
       FVerticesCount := 0;
       FFacesCount := 0;
+      SetLength(FFaces, 0);
       FLastSuccess := False;
       FLastResult := 'Error parsing STL: ' + E.Message;
       Log(llError, FLastResult);
